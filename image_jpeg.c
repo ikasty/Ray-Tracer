f97e0fc1f4ba8aecd442598bb35e90fede3cc339
rpdladps <rpdladps@gmail.com>
2015-05-15 12:22:55 +0900

    RGBA 구조에 맞게 jpeg_read 변경

--- image_jpeg.c
+++ image_jpeg.c
@@ -4,9 +4,10 @@
 // jpeg 포맷 읽기
 int jpeg_read(Data *data, char *filename, int flags){
 	FILE *jpeg_file;
-	unsigned int width, height;
+	unsigned int width, height, components, i;
 	struct jpeg_decompress_struct jpeg_info;
 	struct jpeg_error_mgr jerr;
+	JSAMPARRAY buffer;
 
 	jpeg_info.err = jpeg_std_error(&jerr);
 	jpeg_file = fopen(filename, "rb");
@@ -21,20 +22,31 @@ int jpeg_read(Data *data, char *filename, int flags){
 
 	width = jpeg_info.output_width;
 	height = jpeg_info.output_height;
-	
+	components = jpeg_info.output_components;
+	// RGBA 는 4바이트
+	if(components > 4) components = 4; // 절대 일어나지 않는 일
 	
 	// rgb값을 모두 저장할 수 있도록 rgb_buffer에 공간 할당
-	if(rgb_buffer_calloc(data, height, width * 3))
+	buffer = (JSAMPARRAY)malloc(sizeof(JSAMPROW) * 1);
+	buffer[0] = (JSAMPROW)malloc(sizeof(JSAMPLE) * width * components);
+	if(rgb_buffer_calloc(data, height, width))
 		return -1;
 
 	while (jpeg_info.output_scanline < height) 
 	{
-		jpeg_read_scanlines(&jpeg_info, &data->texture.rgb_buffer[jpeg_info.output_scanline], 1);
+		jpeg_read_scanlines(&jpeg_info, buffer, 1);
+		for(i = 0; i < width; i++) {
+			for(j = 0; j < components; j++) {
+				data->texture.pixels[i][jpeg_info.output_scanline].l[j] = buffer[0][i*components + j];
+			}
+		}
+		// 버그있을듯
 	}
 	data->texture.width = width;
 	data->texture.height = height;
 	fclose(jpeg_file);
 
- return 0;
+	jpeg_destroy_decompress(*jpeg_info);
+	return 0;
 }
 
