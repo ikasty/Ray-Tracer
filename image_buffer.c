4c675a43ea67557180cd62dc8fbc1c3d423c87de
rpdladps <rpdladps@gmail.com>
2015-05-15 10:25:30 +0900

    buffer alloc 부분 분리

---
+++ image_buffer.c
@@ -0,0 +1,18 @@
+// image_buffer.c
+#include "image_buffer.h"
+
+void rgb_buffer_calloc(Data *data, int row, int col)
+{
+	int i;
+
+	data->texture.rgb_buffer = (unsigned char **)calloc(row, sizeof(unsigned char *));
+	if (!data->texture.rgb_buffer)
+		exit(1);
+	for (i = 0; i < row; i++)
+	{
+		data->texture.rgb_buffer[i] = (unsigned char *)calloc(col, sizeof(unsigned char));
+		if (!data->texture.rgb_buffer[i])
+			exit(1);
+	}
+}
+
---
+++ image_buffer.h
@@ -0,0 +1,11 @@
+#ifndef IMAGE_BUFFER_H
+#define IMAGE_BUFFER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+//#include <string.h>
+#include "include/type.h"
+
+void rgb_buffer_calloc(Data *data, int row, int col)
+
+#endif
