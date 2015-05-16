d12bf633380aee415e5cec15dab099f2cb010631
rpdladps <rpdladps@gmail.com>
2015-05-16 22:31:10 +0900

    잘못된 메모리 참조하던 문제 수정

--- image_jpeg.c
+++ image_jpeg.c
@@ -9,6 +9,7 @@ int jpeg_read(Image *image, char *filename, int flags){
 	struct jpeg_error_mgr jerr;
 	JSAMPARRAY buffer;
 
+ PDEBUG("jpeg_read start\n");
 	jpeg_info.err = jpeg_std_error(&jerr);
 	jpeg_file = fopen(filename, "rb");
 	if (jpeg_file == NULL){
@@ -19,7 +20,7 @@ int jpeg_read(Image *image, char *filename, int flags){
 	jpeg_stdio_src(&jpeg_info, jpeg_file);
 	jpeg_read_header(&jpeg_info, TRUE);
 	jpeg_start_decompress(&jpeg_info);
-
+// PDEBUG("jpeg_read phase 1\n");
 	width = jpeg_info.output_width;
 	height = jpeg_info.output_height;
 	components = jpeg_info.output_components;
@@ -29,25 +30,32 @@ int jpeg_read(Image *image, char *filename, int flags){
 	// rgb값을 모두 저장할 수 있도록 rgb_buffer에 공간 할당
 	buffer = (JSAMPARRAY)malloc(sizeof(JSAMPROW) * 1);
 	buffer[0] = (JSAMPROW)malloc(sizeof(JSAMPLE) * width * components);
-	if(rgb_buffer_calloc(image, height, width))
+	if(rgb_buffer_calloc(image, width, height))
 		return -1;
 
-	while (jpeg_info.output_scanline < height) 
+ image_reset(image);
+
+// PDEBUG("jpeg_read phase 2\n");
+	while (jpeg_info.output_scanline < height - 1) 
 	{
+//  PDEBUG("jpeg] scanline start at %d\n", jpeg_info.output_scanline);
 		jpeg_read_scanlines(&jpeg_info, buffer, 1);
 		for(i = 0; i < width; i++) {
 			for(j = 0; j < components; j++) {
+//    PDEBUG("jpeg] scanline : %d, width : %d, height : %d, counter : %d+%d\n", jpeg_info.output_scanline, width, height, i, j);
 				image->pixels[jpeg_info.output_scanline][i].l[j] = buffer[0][i*components + j];
 			}
 		}
-		// 버그있을듯
+//  PDEBUG("jpeg] scanline end at %d\n", jpeg_info.output_scanline);
 	}
 
+// PDEBUG("jpeg_read phase 3\n");
  image->width = width;
 	image->height = height;
 	fclose(jpeg_file);
 
 	jpeg_destroy_decompress(&jpeg_info);
+// PDEBUG("jpeg_read end\n");
 	return 0;
 }
 
