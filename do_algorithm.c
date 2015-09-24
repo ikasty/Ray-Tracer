a33d1db93c8ce7e4672604c5ef4c53c62f5b7026
ikasty <mail.ikasty@gmail.com>
2015-09-24 16:35:24 +0900

    알고리즘을 수행하는 파트를 분리함

---
+++ do_algorithm.c
@@ -0,0 +1,70 @@
+﻿#include "include/type.h"
+#include "settings.h"
+#include "timecheck.h"
+
+#include "gen_ray.h"
+
+// include search and render algorithm
+#include "algorithms.h"
+
+// shading algorithm
+#include "shading/shading.h"
+
+#include "do_algorithm.h"
+
+void do_algorithm(Data *data, Image *screen_buffer, void (*print_percent)(float percent))
+{
+	int index_x, index_y; // 스크린의 픽셀별로 통과하는 광선의 x, y축 좌표 인덱스
+
+	USE_SCREEN(screen);
+	USE_TIMECHECK();
+
+	// 만약 가속구조체를 사용한다면 빌드함
+	if (accel_build)
+	{
+		// 기존 구조체 해제
+		if (clear_accel) (*clear_accel)(data);
+
+		TIMECHECK_START();
+		(*accel_build)(data);
+		TIMECHECK_END(build_clock);
+
+		PDEBUG("accel_build finished\n");
+	}
+
+	// 각 픽셀별로 교차검사를 수행합니다.
+	for (index_y = 0; index_y < screen->ysize; index_y++)
+	{
+		float percent;
+
+		// 진행 상태 출력
+		if (print_percent)
+		{
+			percent = ((float)index_y / screen->ysize) * 100.0f;
+			(*print_percent)(percent);
+		}
+
+		for (index_x = 0; index_x < screen->xsize; index_x++)
+		{
+			Ray f_ray = gen_ray((float)index_x, (float)index_y);
+			Hit ist_hit;
+
+			// 현재 광선에서 교차검사를 수행함
+			TIMECHECK_START();
+			ist_hit = (*intersect_search)(data, &f_ray);
+			TIMECHECK_END(search_clock);
+
+			// bmp파일을 작성에 필요한 색상정보를 입력합니다.
+			if (ist_hit.t > 0)
+			{
+				//unsigned int *pixel = &screen_buffer[screen->xsize * index_y + index_x];
+				RGBA *pixel = &(screen_buffer->pixels[index_y][index_x]);
+
+				// 교차된 Primitive가 있다면 렌더링함
+				TIMECHECK_START();
+				*pixel = shading(f_ray, data->primitives[ist_hit.prim_id], ist_hit, data);
+				TIMECHECK_END(render_clock);
+			}
+		} // index_x
+	} // index_y
+}
--- main.c
+++ main.c
@@ -40,8 +40,8 @@ either expressed or implied, of the FreeBSD Project.
 // include search and render algorithm
 #include "algorithms.h"
 
-// shading algorithm
-#include "shading/shading.h"
+// do algorithm
+#include "do_algorithm.h"
 
 #include "obj_transform.h"
 #include "timecheck.h"
@@ -58,7 +58,7 @@ PDEBUG_INIT();
 static int use_opengl = true;
 
 // 콘솔 화면에 진행상황을 출력해 줍니다.
-static void print_percent(int frame_number, float percent)
+static void print_percent(float percent)
 {
 	int i;
 	USE_SCREEN(screen);
@@ -69,7 +69,10 @@ static void print_percent(int frame_number, float percent)
 		__PDEBUG_ENABLED = 0;
 	});
 
-	printf("\rframe %02d/%02d: [", frame_number, screen->frame_count);
+	printf("\rframe %02d/%02d: [", screen->frame_number, screen->frame_count);
+
+	// frame 개수를 고려한 percent 계산
+	percent = (percent + screen->frame_number * 100) / screen->frame_count;
 
 	for (i = 0; i <= (int)(percent / 5); i++) printf("=");
 	for (i = (int)(percent / 5); i < 20; i++) printf(" ");
@@ -90,63 +93,6 @@ static void print_percent(int frame_number, float percent)
 			get_total_clock());
 	}
 	fflush(stdout);
-}
-
-static void do_algorithm(Data *data, Image* screen_buffer)
-{
-	int index_x, index_y; // 스크린의 픽셀별로 통과하는 광선의 x, y축 좌표 인덱스
-
-	USE_SCREEN(screen);
-	USE_TIMECHECK();
-
-	// 만약 가속구조체를 사용한다면 빌드함
-	if (accel_build)
-	{
-		// 기존 구조체 해제
-		if (clear_accel) (*clear_accel)(data);
-
-		TIMECHECK_START();
-		(*accel_build)(data);
-		TIMECHECK_END(build_clock);
-
-		PDEBUG("accel_build finished\n");
-	}
-
-	// 각 픽셀별로 교차검사를 수행합니다.
-	for (index_y = 0; index_y < screen->ysize; index_y++)
-	{
-		float percent;
-
-		// 진행 상태 출력
-		if (!use_opengl)
-		{
-			percent = ((float)index_y / screen->ysize + screen->frame_number) / screen->frame_count * 100.0f;
-			print_percent(screen->frame_number, percent);
-		}
-		
-		for (index_x = 0; index_x < screen->xsize; index_x++)
-		{
-			Ray f_ray = gen_ray((float)index_x, (float)index_y);
-			Hit ist_hit;
-
-			// 현재 광선에서 교차검사를 수행함
-			TIMECHECK_START();
-			ist_hit = (*intersect_search)(data, &f_ray);
-			TIMECHECK_END(search_clock);
-
-			// bmp파일을 작성에 필요한 색상정보를 입력합니다.
-			if (ist_hit.t > 0)
-			{
-				//unsigned int *pixel = &screen_buffer[screen->xsize * index_y + index_x];
-				RGBA *pixel = &(screen_buffer->pixels[index_y][index_x]);
-
-				// 교차된 Primitive가 있다면 렌더링함
-				TIMECHECK_START();
-				*pixel = shading(f_ray, data->primitives[ist_hit.prim_id], ist_hit, data);
-				TIMECHECK_END(render_clock);
-			}
-		} // index_x
-	} // index_y
 }
 
 static void do_console(Data *data, char *input_file)
@@ -181,7 +127,7 @@ static void do_console(Data *data, char *input_file)
 		//// -- execute phase --
 		PDEBUG("main.c execute phase\n");
 		screen->frame_number = frame_number;
-		do_algorithm(data, screen_buffer);
+		do_algorithm(data, screen_buffer, &print_percent);
 
 		//// -- post-step phase --
 		PDEBUG("main.c post-step phase\n");
@@ -194,7 +140,8 @@ static void do_console(Data *data, char *input_file)
 		image_write(screen_buffer, output_file, IMAGE_NO_FLAGS);
 	} // index_x
 
-	print_percent(frame_number, 100.0f);
+	screen->frame_number = screen->frame_count;
+	print_percent(100.0f);
 
 	free(screen_buffer);
 }
